60d8561a255708fd24aa958fe85f7010ed8350b7
Gleidson Medeiros <gleidsonlm@gmail.com>
2025-06-21 07:26:54 -0300

    feat: Xcode init project

---
+++ businesscard/businesscardApp.swift
@@ -0,0 +1,17 @@
+//
+//  businesscardApp.swift
+//  businesscard
+//
+//  Created by Gleidson M on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct businesscardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
