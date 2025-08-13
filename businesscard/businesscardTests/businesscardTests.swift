dd2917b954c1aa10a98191c5e7821d0780bac4b0
Gleidson Medeiros <gleidsonlm@gmail.com>
2025-08-13 15:41:58 -0300

    init: Xcode project start

---
+++ businesscard/businesscardTests/businesscardTests.swift
@@ -0,0 +1,16 @@
+//
+//  businesscardTests.swift
+//  businesscardTests
+//
+//  Created by Gleidson L Medeiros on 13/08/25.
+//
+
+import Testing
+
+struct businesscardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
